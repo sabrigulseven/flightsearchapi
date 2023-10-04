be46c5e110b6b8bafcac061cfe134f9772cf58c9
Sabri Gülseven <sabrigulseven@outlook.com>
2023-10-05 00:38:17 +0300

    Spring Security migrated to spring boot 3.1

--- src/main/java/com/sabrigulseven/flight/config/SecurityConfiguration.java
+++ src/main/java/com/sabrigulseven/flight/config/SecurityConfiguration.java
@@ -2,52 +2,57 @@ package com.sabrigulseven.flight.config;
 
 
 import com.sabrigulseven.flight.model.UserRole;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/swagger-ui.html",
+            "swagger-ui/index.html",
+            "/swagger-resources/**",
+            "/swagger-resources",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/configuration/**",
+            "/webjars/**",
+            "/swagger-ui/**",
+            "/h2-console"};
+
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/auth/**",
-                        "/swagger-ui.html",
-                        "swagger-ui/index.html",
-                        "/swagger-resources/**",
-                        "/swagger-resources",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/configuration/**",
-                        "/webjars/**",
-                        "/swagger-ui/**",
-                        "/h2-console")
-                .permitAll()
-                .requestMatchers("/api/v1/admin/**").hasRole(UserRole.ADMIN.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers(antMatcher("/api/v1/admin/**")).hasRole(UserRole.ADMIN.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 }
